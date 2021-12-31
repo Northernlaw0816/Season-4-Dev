7bb46a71fafa638b250a977eb5e840aff62894b5
RishiMenon2004 <rishimenon82004@gmail.com>
2021-12-31 23:54:35 +0530

    change the link hover effect

--- public/componets/Footer.tsx
+++ public/componets/Footer.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 
 //stylesheets
 import styles from "../../styles/Footer.module.scss"
+import effects from "../../styles/Effects.module.scss"
 //assets
 import YuvaLarge from '../../public/images/footer_logo_color.png'
 import YuvaSingle from '../../public/images/footer_logo_color_single.png'
@@ -22,24 +23,23 @@ const Footer = () => {
         <div className={styles.footer_content}>
           <label style={{gridArea: "event"}}>Events</label>
           <div className={styles.footer_links} style={{gridArea: "event_links"}}>
-            <Link href="/events/arena-of-valor"><a role="link">Arena of Valor</a></Link>
-            <Link href="/events/knock-out"><a role="link">Knock Out</a></Link>
-            <Link href="/events/truth-or-debug"><a role="link">Truth or Debug</a></Link>
-            <Link href="/events/log-and-blog"><a role="link">Log and Blog</a></Link>
-            <Link href="/events/designscape"><a role="link">Designscape</a></Link>
-            <Link href="/events/otakuiz"><a role="link">Otakuiz</a></Link>
+            <Link href="/events/arena-of-valor"><a role="link" className={effects.link_hover_effect}>Arena of Valor</a></Link>
+            <Link href="/events/knock-out"><a role="link" className={effects.link_hover_effect}>Knock Out</a></Link>
+            <Link href="/events/truth-or-debug"><a role="link" className={effects.link_hover_effect}>Truth or Debug</a></Link>
+            <Link href="/events/log-and-blog"><a role="link" className={effects.link_hover_effect}>Log and Blog</a></Link>
+            <Link href="/events/designscape"><a role="link" className={effects.link_hover_effect}>Designscape</a></Link>
+            <Link href="/events/otakuiz"><a role="link" className={effects.link_hover_effect}>Otakuiz</a></Link>
           </div>
           <label style={{gridArea: "links_title"}}>Links</label>
           <div className={styles.footer_links} style={{gridArea: "links_links"}}>
-            <Link href="/"><a role="link">Gallery</a></Link>
-            <Link href="/"><a role="link">Details</a></Link>
-            <Link href="/"><a role="link">Register Here</a></Link>
-            <Link href="/"><a role="link">About</a></Link>
-            <Link href="/"><a role="link">Contact</a></Link>
+            <Link href="/"><a role="link" className={effects.link_hover_effect}>About</a></Link>
+            <Link href="/"><a role="link" className={effects.link_hover_effect}>Registeration</a></Link>
+            <Link href="/"><a role="link" className={effects.link_hover_effect}>Gallery</a></Link>
+            <Link href="/"><a role="link" className={effects.link_hover_effect}>Contact</a></Link>
           </div>
         </div>
         <div className={styles.footer_details}>
-          <p>Arranged by 11th Grade</p>
+          <p>An Initiative By The Students For The Students</p>
           <p>E-Mail: <a role="link" href="mailto:nutopia.ybps@gmail.com">nutopia.ybps@gmail.com</a></p>
         </div>
         <div className={styles.footer_copyright}>
