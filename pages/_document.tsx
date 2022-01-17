3d38cd1ae8c6a810678f3b3c5ef40ea9fc05c5e5
RishiMenon2004 <rishimenon82004@gmail.com>
2022-01-17 19:24:02 +0530

    add twitter:title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,16 +5,24 @@ export default class MyDocument extends Document {
         return <Html lang="en">
             <Head>
                 <link rel="icon" href="/favicon.svg" />
+                {/* Twitter */}
                 <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:title" content="NuTopia" />
+                <meta property="twitter:description" content="Offical NuTopia Website | Season 1" />
+                <meta property="twitter:image" content="https://nutopia-beta.vercel.app/images/home-gallery/1.jpg" />
+                
+                {/* Open Graph */}
                 <meta property="og:locale" content="en-US" />
                 <meta property="og:type" content="website" />
-                <meta property="og:title" content="NuTopia" />
                 <meta property="og:site_name" content="NuTopia" />
                 <meta property="og:url" content="https://www/nutopia.in/" />
-                <meta name="description" content="Offical NuTopia Website | Season 1" />
+                <meta property="og:title" content="NuTopia" />
                 <meta property="og:description" content="Offical NuTopia Website | Season 1" />
-                <meta data-react-helmet="true" property="og:image" content="https://nutopia-beta.vercel.app/images/home-gallery/1.jpg" />
-                <meta data-react-helmet="true" property="twitter:image" content="https://nutopia-beta.vercel.app/images/home-gallery/1.jpg" />
+                <meta property="og:image" content="https://nutopia-beta.vercel.app/images/home-gallery/1.jpg" />
+                
+                {/* Default */}
+                <meta name="title" content="NuTopia" />
+                <meta name="description" content="Offical NuTopia Website | Season 1" />
             </Head>
 
             <body>
