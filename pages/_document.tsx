64b7eea91ca8921a8c0683d379518d98fed672a5
RishiMenon2004 <rishimenon82004@gmail.com>
2022-01-17 18:47:23 +0530

    fix og:image problem

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
             <Head>
                 <link rel="icon" href="/favicon.svg" />
                 <meta name="description" content="Offical NuTopia Website | Season 1" />
-                <meta property="og:image" content="/images/home-gallery/1.jpg" />
+                <meta property="og:image" content="../public/images/home-gallery/1.jpg" />
                 <meta property="og:url" content="https://www/nutopia.in/" />
                 <meta property="og:title" content="NuTopia" />
                 <meta property="og:description" content="Offical NuTopia Website | Season 1" />
