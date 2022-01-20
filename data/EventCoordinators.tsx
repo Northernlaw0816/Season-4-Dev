4339fa15bbe7d92fffa111da3074ce06998216b3
RishiMenon2004 <rishimenon82004@gmail.com>
2022-01-20 11:16:30 +0530

    add shabesa and marudhu profile image

--- data/EventCoordinators.tsx
+++ data/EventCoordinators.tsx
@@ -5,115 +5,117 @@ import samer from "../public/images/profile/samer.png"
 import sanjaySR from "../public/images/profile/sanjay_sr.png"
 import sanjayAR from "../public/images/profile/sanjay_ar.png"
 import snigdha from "../public/images/profile/snigdha.png"
+import marudhu from "../public/images/profile/marudhu.png"
+import shabesa from "../public/images/profile/shabesa.png"
 import neha from "../public/images/profile/neha.png"
 import aadvika from "../public/images/profile/aadvika.png"
 import akshay from "../public/images/profile/akshay.png"
 import diya from "../public/images/profile/diya.png"
 
 const EventHeads = [
-    {
-        name: "Sanjay A.R.",
-        grade: "11",
-        src: sanjayAR
-    },
-    {
-        name: "Shakthi Kumar S.",
-        grade: "11",
-        src: shakti
-    }
+	{
+		name: "Sanjay A.R.",
+		grade: "11",
+		src: sanjayAR
+	},
+	{
+		name: "Shakthi Kumar S.",
+		grade: "11",
+		src: shakti
+	}
 ]
 
 const EventCoordinators = [
-    {
-        name: "Sidharth B.P.",
-        event: "Arena of Valor - Console",
-        grade: "12",
-        src: sidharth
-    },
-    {
-        name: "Shakthi Kumar S.",
-        event: "Arena of Valor - PC",
-        grade: "11",
-        src: shakti
-    },
-    {
-        name: "Ananya Bansal",
-        event: "Arena of Valor - PC",
-        grade: "11",
-        src: placeholder
-    },
-    {
-        name: "Samer Rahmaan R.",
-        event: "Arena of Valor - Mobile",
-        grade: "12",
-        src: samer
-    },
-    {
-        name: "Sanjay Shriram S.",
-        event: "Arena of Valor - Mobile",
-        grade: "11",
-        src: sanjaySR
-    },
-    {
-        name: "Sanjay A.R.",
-        event: "Knock Out",
-        grade: "11",
-        src: sanjayAR
-    },
-    {
-        name: "Snigdha S. K.",
-        event: "Knock Out",
-        grade: "11",
-        src: snigdha
-    },
-    {
-        name: "Marudhu Paandian K.",
-        event: "Truth or Debug",
-        grade: "12",
-        src: placeholder
-    },
-    {
-        name: "Shabesa K. A.",
-        event: "Truth or Debug",
-        grade: "12",
-        src: placeholder
-    },
-    {
-        name: "Aadvika R.",
-        event: "Log and Blog",
-        grade: "11",
-        src: aadvika
-    },
-    {
-        name: "Neha Julius Panakkal",
-        event: "Log and Blog",
-        grade: "11",
-        src: neha
-    },
-    {
-        name: "Rohit Kanna S.",
-        event: "Designscape",
-        grade: "1",
-        src: placeholder
-    },
-    {
-        name: "Akshay Balaji S.",
-        event: "Designscape",
-        grade: "11",
-        src: akshay
-    },
-    {
-        name: "Mathangi A. R.",
-        event: "Otakuiz",
-        grade: "11",
-        src: placeholder
-    },
-    {
-        name: "Diya Tantia S.",
-        event: "Otakuiz",
-        grade: "12",
-        src: diya
-    }
+	{
+		name: "Sidharth B.P.",
+		event: "Arena of Valor - Console",
+		grade: "12",
+		src: sidharth
+	},
+	{
+		name: "Shakthi Kumar S.",
+		event: "Arena of Valor - PC",
+		grade: "11",
+		src: shakti
+	},
+	{
+		name: "Ananya Bansal",
+		event: "Arena of Valor - PC",
+		grade: "11",
+		src: placeholder
+	},
+	{
+		name: "Samer Rahmaan R.",
+		event: "Arena of Valor - Mobile",
+		grade: "12",
+		src: samer
+	},
+	{
+		name: "Sanjay Shriram S.",
+		event: "Arena of Valor - Mobile",
+		grade: "11",
+		src: sanjaySR
+	},
+	{
+		name: "Sanjay A.R.",
+		event: "Knock Out",
+		grade: "11",
+		src: sanjayAR
+	},
+	{
+		name: "Snigdha S. K.",
+		event: "Knock Out",
+		grade: "11",
+		src: snigdha
+	},
+	{
+		name: "Marudhu Paandian K.",
+		event: "Truth or Debug",
+		grade: "12",
+		src: marudhu
+	},
+	{
+		name: "Shabesa K. A.",
+		event: "Truth or Debug",
+		grade: "12",
+		src: shabesa
+	},
+	{
+		name: "Aadvika R.",
+		event: "Log and Blog",
+		grade: "11",
+		src: aadvika
+	},
+	{
+		name: "Neha Julius Panakkal",
+		event: "Log and Blog",
+		grade: "11",
+		src: neha
+	},
+	{
+		name: "Rohit Kanna S.",
+		event: "Designscape",
+		grade: "1",
+		src: placeholder
+	},
+	{
+		name: "Akshay Balaji S.",
+		event: "Designscape",
+		grade: "11",
+		src: akshay
+	},
+	{
+		name: "Mathangi A. R.",
+		event: "Otakuiz",
+		grade: "11",
+		src: placeholder
+	},
+	{
+		name: "Diya Tantia S.",
+		event: "Otakuiz",
+		grade: "12",
+		src: diya
+	}
 ]
 
 export {EventCoordinators as EventCoordinators, EventHeads as EventHeads}
