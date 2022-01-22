cb50ecf02536942f5487f2014c1219d8779fa338
RishiMenon2004 <rishimenon82004@gmail.com>
2022-01-23 01:28:31 +0530

    remove quote from about

--- data/pages/events/otakuiz.tsx
+++ data/pages/events/otakuiz.tsx
@@ -4,7 +4,7 @@ const Otakuiz = {
     tagline: "You don’t stand a single chance to win unless you fight!",
     
     headings: {
-        about: "Aesthetically pleasing scenes, powerful dialogues and stellar battles, anime has always been a world charged with energy and emotion. For all the seasoned anime watchers out there, this is the perfect opportunity for you. Prepare to battle it out in Otakuiz! An event that will test your knowledge in anime every step of the way. Do you have what it takes? In the clever words of Mikasa Ackerman, \“You don’t stand a single chance to win unless you fight\”.",
+        about: "Aesthetically pleasing scenes, powerful dialogues and stellar battles, anime has always been a world charged with energy and emotion. For all the seasoned anime watchers out there, this is the perfect opportunity for you. Prepare to battle it out in Otakuiz! An event that will test your knowledge in anime every step of the way. Do you have what it takes?",
             
         rules: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum nulla quasi esse illo dolores ipsa ipsum? Odio unde repellat assumenda nulla laborum nesciunt quos illum ad sed explicabo, quam asperiores?",
         
