a90e6e10cd3ba5e4da3570fd671d4ba3cf1435a5
RishiMenon2004 <rishimenon82004@gmail.com>
2022-01-23 01:28:39 +0530

    add names to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -76,7 +76,7 @@ const Footer = () => {
           
           <div className={styles.separator}></div>
           
-          <div><a role="link" className={styles.TDB} href="https://sites.google.com/view/team-de-bug"><Image src={TDB} alt="team debug logo" width={24} height={24} /></a>Website by Team De-Bug</div>
+          <div><a role="link" className={styles.TDB} href="https://sites.google.com/view/team-de-bug"><Image src={TDB} alt="team debug logo" width={24} height={24} /></a>Website by Team De-Bug: Marudhu Paandian K., Rishi Menon and Shabesa K. A.</div>
         </div>
       </footer>
     )
