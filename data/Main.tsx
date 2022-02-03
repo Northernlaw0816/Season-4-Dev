30d9d0657a77efb94101dddc88e89e609ec80a13
RishiMenon2004 <rishimenon82004@gmail.com>
2022-02-03 11:30:50 +0530

    italics tagline

--- data/Main.tsx
+++ data/Main.tsx
@@ -1,7 +1,11 @@
 const Main = {
     year: 2022,
     season: 1,
-    tagline: "A Hub For Cyber Geeks!"
+    tagline: "A Hub For Cyber Geeks!",
+    dates: {
+        day1: new Date(2022, 1, 8).toDateString().split(" ").slice(1).join(" "),
+        day2: new Date(2022, 1, 9).toDateString().split(" ").slice(1).join(" ")
+    }
 }
 
 export default Main
