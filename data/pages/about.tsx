d4d7eb8dd87216103bfb585b9eb6b67478519ebb
RishiMenon2004 <rishimenon82004@gmail.com>
2022-02-04 02:02:08 +0530

    add hyphen

--- data/pages/about.tsx
+++ data/pages/about.tsx
@@ -1,7 +1,7 @@
 const About = {
     whoWeAre: [
         <>With an algorithm to revel the joie de vivre of Science, <a href="https://yuvabharathi.in">Yuvabharathi Public School</a> has set its sails to NuTopia, ‟A Hub For Cyber Geeks!”. NuTopia is a tech-powered STEM carnival conducted by Yuvabharathians where Knowledge, Creativity and Skills of the cyber-age are celebrated.</>,
-        "Season 1 of NuTopia, 2022, presents six scintillating events to enthral your grey cells and keep your senses engrossed in scientific quests.",
+        "Season 1 of NuTopia - 2022, presents six scintillating events to enthral your grey cells and keep your senses engrossed in scientific quests.",
         "Press Enter and join the network to make your presence known!"
     ]
 }
