dab2082299e74f6b4fdc8900c4091f4e248eab98
RishiMenon2004 <rishimenon82004@gmail.com>
2022-02-03 11:32:21 +0530

    embed trailer

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import Head from 'next/head'
 //components
 import Layout from '../components/Layout'
 import HomeBanner from '../components/HomeBanner'
-import HomeGallery from '../components/HomeGallery'
 //stylesheets
 import styles from '../styles/pages/Home.module.scss'
 //data
@@ -20,8 +19,9 @@ const Home: NextPage = () => {
       <Layout skipTo="#content" overrideClasses={styles.main}>
         <HomeBanner/>
         {/* <HomeGallery/> */}
-        <div>
-          {/* Trailer */}
+        <div className={styles.content_container}>
+          <iframe className={styles.video_embed} src="https://www.youtube-nocookie.com/embed/cvLqHBbXSKE" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+          <hr />
           <h1>Welcome to NuTopia</h1>
           {/* content */}
           {/* event updates */}
