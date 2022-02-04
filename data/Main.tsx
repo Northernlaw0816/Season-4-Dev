910aa6a7475462831a0d552c718f40752a1e9003
RishiMenon2004 <rishimenon82004@gmail.com>
2022-02-04 23:03:06 +0530

    change time

--- data/Main.tsx
+++ data/Main.tsx
@@ -6,7 +6,7 @@ const Main = {
         day1: new Date(2022, 1, 8).toDateString().split(" ").slice(1).join(" "),
         day2: new Date(2022, 1, 9).toDateString().split(" ").slice(1).join(" ")
     },
-    updateMessage: "IMPORTANT INFORMATION: LAST DATE FOR REGISTRATION - FEBRUARY 6 2022, 5 PM"
+    updateMessage: "IMPORTANT INFORMATION: LAST DATE FOR REGISTRATION - FEBRUARY 6 2022, 3 PM"
 }
 
 export default Main
