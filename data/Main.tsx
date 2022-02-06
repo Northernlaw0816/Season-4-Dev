67176fe4e1a54c14029ccae14b15836ef5c0fec4
RishiMenon2004 <rishimenon82004@gmail.com>
2022-02-06 21:38:22 +0530

    relink brochure

--- data/Main.tsx
+++ data/Main.tsx
@@ -9,6 +9,8 @@ const Main = {
         day2: new Date(2022, 1, 9).toDateString().split(" ").slice(1).join(" ")
     },
     registrationClosingDate: date, // month is 0 indexed; Jan is 0;
+    brochureDownload: "https://drive.google.com/u/0/uc?id=1ceSZtWD9UyQy3UQBFprOEDmPUGyrOZTu&export=download",
+    brochurePreview: "https://drive.google.com/file/d/1ceSZtWD9UyQy3UQBFprOEDmPUGyrOZTu/preview",
     updateMessage: date.getTime() <= new Date().getTime() ? "IMPORTANT INFORMATION: REGISTRATIONS ARE CLOSED | BACKGROUND GUIDE FOR KNOCK OUT IS RELEASED" : "IMPORTANT INFORMATION: LAST DATE FOR REGISTRATION - FEBRUARY 6 2022, 3 PM"
 }
 
