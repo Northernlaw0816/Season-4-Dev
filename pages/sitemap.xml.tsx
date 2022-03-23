7f62f60e9587848a9cabfb2fdc92a6829f3ae21e
RishiMenon2004 <rishimenon82004@gmail.com>
2022-03-23 17:02:09 +0530

    added small change to fix issues with vercel

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -9,9 +9,11 @@ const Sitemap = () => {
 export const getServerSideProps = async ({ res }: any) => {
 	const BASE_URL = 'https://nutopia.in'
 
-	const staticPaths = fs
-		.readdirSync("pages")
-		.filter((staticPage) => {
+	const staticPaths = fs.readdirSync({
+		development: 'pages',
+		production: './',
+		test: 'pages'
+	}[process.env.NODE_ENV]).filter((staticPage) => {
 		return ![
 			"api",
 			"_app.tsx",
@@ -19,29 +21,23 @@ export const getServerSideProps = async ({ res }: any) => {
 			"404.tsx",
 			"sitemap.xml.tsx",
 		].includes(staticPage);
-		})
-		.map((staticPagePath) => {
+	}).map((staticPagePath) => {
 		return `${BASE_URL}/${staticPagePath}`
-		})
+	})
 
 	const allPaths = [ ...staticPaths ]
 
-	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-		<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-			${
-				allPaths.map((url) => {
-					return `
-						<url>
-							<loc>${url}</loc>
-							<lastmod>${new Date().toISOString()}</lastmod>
-							<changefreq>monthly</changefreq>
-							<priority>1.0</priority>
-						</url>
-					`
-				}).join("")
-			}
-		</urlset>
-	`
+	const sitemap =
+	(`<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+		${allPaths.map((url) => { return (
+			`<url>
+				<loc>${url}</loc>
+				<lastmod>${new Date().toISOString()}</lastmod>
+				<changefreq>monthly</changefreq>
+				<priority>1.0</priority>
+			</url>`
+		)}).join("")}
+	</urlset>`)
 
 	res.setHeader('Content-Type', 'text/xml')
 	res.write(sitemap)
