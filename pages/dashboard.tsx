b5fd698f9df9d9edd841a7be03f9a0ce588121eb
RishiMenon2004 <rishimenon82004@gmail.com>
2022-07-10 02:59:59 +0530

    style dashboard page

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,10 +2,11 @@
 import HeadTemplate from "../components/HeadTemplate"
 import Layout from "../components/Layout"
 
-import styles from '../styles/pages/Contact.module.scss'
+import styles from '../styles/pages/Dashboard.module.scss'
 import { useEffect, useState } from "react"
 import { collection, getDocs, query, where } from "firebase/firestore"
 import { firestore } from "../firebase/clientApp"
+import { titleCase } from "../functions"
 
 export const Dashboard = () => {
 
@@ -14,56 +15,119 @@ export const Dashboard = () => {
 		schoolId: "",
 		email: ""
 	})
-	
-	useEffect(() => {
-		getUserData()
-	}, [])
 
-	const getUserData = async () => { 
-		const data = await fetch('https://api.nutopia.in/user', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-				"Access-Control-Allow-Origin": "*",
-			},
-			body: JSON.stringify({
-				userToken: localStorage.getItem('userToken')
-			})
-		}).then(response => response.json()).then(data => {return data})
+	const [userRegistrations, setUserRegistrations] = useState<any>([])
 
-		if (data.success) {
-			console.log(data)
-			setUserData(data.user)
+	const getUserData = () => { 
+		const data = {
+			schoolName: localStorage.getItem("schoolName"),
+			schoolId: localStorage.getItem("schoolId"),
+			email: localStorage.getItem("email")
 		}
+
+		setUserData(data)
 	}
 
 	const getUserRegistrations = async () => {
 		const registrationsCollection = collection(firestore, "registrations_season_2")
 
-		const userDocQuery = query(registrationsCollection, where("userId", "==", userData.userId))
+		const userDocQuery = query(registrationsCollection, where("schoolId", "==", userData.schoolId))
 
 		const userDocumentsSnapshot = await getDocs(userDocQuery)
 
-		const userDocuments = userDocumentsSnapshot.docs.map(doc => {
-			return doc
+		const userDocuments: any[] = [] 
+
+		userDocumentsSnapshot.forEach(doc => {
+			return userDocuments.push(doc)
 		})
 
-
+		setUserRegistrations(userDocuments)
 	}
+	
+	useEffect(() => {
+		getUserData()
+	}, [])
+
+	useEffect(() => {
+		getUserRegistrations()
+	}, [userData])
 
     return (<>
         <HeadTemplate title="NuTopia | Contact Us" description="View Our Contact Information"/>
         <Layout overrideClasses={styles.main}>
-			<div>
-				<h2>Your School</h2>
-				<p>School Name: {userData.schoolName}</p>
-				<p>School ID: {userData.schoolId}</p>
-				<p>School Email: {userData.email}</p>
-			</div>
 
-			<div>
-				<h2>Your Registrations</h2>
+			<h1>Dashboard</h1>
 
+			<div className={styles.layout}>
+				<div className={styles.user_card}>
+					<h2>Your School</h2>
+					<h3>School Name:</h3> <p>{userData.schoolName}</p>
+					<h3>School ID:</h3> <p>{userData.schoolId}</p>
+					<h3>School Email:</h3> <p>{userData.email}</p>
+				</div>
+				<div className={styles.registrations_container}>
+					<h2>Your Registrations</h2>
+					<div className={styles.card_container}>
+					{userRegistrations.map((registration:any, index:number) => {
+						let registrationData = registration.data()
+
+						let game = <></>			
+				
+						return (
+							<div className={styles.registration_card} key={index}>
+								<h3>{titleCase(registrationData.event.replaceAll("-", " "))}{registrationData.event === "arena-of-valor" && ` - ${titleCase(registrationData.platform)}`}</h3>
+								{registrationData.teams && <p><strong>Teams</strong></p>}
+								<div className={styles.teams_container}>
+									{registrationData.teams && registrationData.teams.map((team:any, index:number) => {
+									
+										if (registrationData.event === "arena-of-valor") {
+											if (registrationData.platform === "pc") {
+												game = <p><strong>{index === 0 ? "Valorant" : "CS:GO"}</strong></p>
+											} else if (registrationData.platform === "console") {
+												game = <p><strong>{index === 0 ? "Fortnite" : "Rocket League"}</strong></p>
+											} else if (registrationData.platform === "mobile"){
+												game = <p><strong>{index < 2 ? "Call of Duty" : "BGMI"}</strong></p>
+											}
+										}
+										return (<>
+											{index > 0 && <hr/>}
+											{game}
+											<div key={index} style={{textAlign: "left"}}>
+									
+												<p><strong>Team Name:</strong> {team.teamName}</p>
+												<div>
+													{team.participants.map((participant:any, index:any) => {
+														return (<>
+															<div key={index} style={{margin: "0 1em"}}>
+																{index > 0 && <hr/>}
+																<p><strong>Participant {index+1}:</strong> {titleCase(participant.name)}</p>
+																<p><strong>Grade:</strong> {participant.grade}</p>
+																<p><strong>Phone:</strong> {participant.phone}</p>
+															</div>
+														</>)
+													})}
+												</div>
+											</div>
+										</>)
+									})}
+								</div>
+								
+								{registrationData.participants && <p><strong>Participants</strong></p>}
+								{registrationData.participants && registrationData.participants.map((participant:any, index:any) => {
+									return (<>
+										<div key={index} style={{textAlign: "left"}}>
+											{index > 0 && <hr/>}
+											<p><strong>Participant {index+1}:</strong> {titleCase(participant.name)}</p>
+											<p><strong>Grade:</strong> {participant.grade}</p>
+											<p><strong>Phone:</strong> {participant.phone}</p>
+										</div>
+									</>)
+								})}
+							</div>
+						)
+					})}
+					</div>
+				</div>
 			</div>
 
 		</Layout>
