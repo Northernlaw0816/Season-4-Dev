432e9d4283489caaf852c27c80401c2f76ab0d67
R-Rajaneesh <rajaneeshr2006@gmail.com>
2022-07-10 20:14:50 +0530

    changed code 400 to 200 and fixed registration stuff

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,4 +1,4 @@
-import { scryptSync, timingSafeEqual, randomBytes } from "crypto";
+import { randomBytes, scryptSync, timingSafeEqual } from "crypto";
 import { collection, doc, getDocs, query, updateDoc, where } from "firebase/firestore";
 import { firestore } from "../../firebase/clientApp";
 
@@ -6,7 +6,7 @@ export default async function handler(req: any, res: any) {
   let success = false;
 
   if (!req.body) {
-    return res.status(400).json({
+    return res.status(200).json({
       success,
       message: "No body",
     });
@@ -27,31 +27,29 @@ export default async function handler(req: any, res: any) {
   });
 
   if (accounts.length === 0) {
-    success = false
-    return res.status(400).json({
+    success = false;
+    return res.status(200).json({
       success,
       message: "SchoolID is incorrect. Please try again",
     });
   } else {
-    success = true
+    success = true;
   }
 
   let userTokens: any[] = [];
 
   accounts.forEach((usr) => {
-      userTokens.push(usr.get("userToken"));
+    userTokens.push(usr.get("userToken"));
   });
 
   if (userTokens.length > 0 && userTokens[0] !== undefined) {
-
-    success = false
-    return res.status(400).json({
+    success = false;
+    return res.status(200).json({
       success: false,
       message: "Already logged in on a different device",
     });
-    
   } else {
-    success = true
+    success = true;
   }
 
   const user = accounts[0];
@@ -65,8 +63,8 @@ export default async function handler(req: any, res: any) {
   let passwordMatch = timingSafeEqual(hashedBuffer, keyBuffer);
 
   if (!passwordMatch) {
-    success = false
-    return res.status(400).json({
+    success = false;
+    return res.status(200).json({
       success: false,
       message: "Password is incorrect. Please try again",
     });
@@ -81,7 +79,7 @@ export default async function handler(req: any, res: any) {
       });
       return res.status(200).json({ message: "Successfully logged in", success: true, userToken: UserToken });
     } catch (err) {
-      console.error(err)
+      console.error(err);
       return res.status(500).json({ message: "Couldn't log in. Please try again", success: false });
     }
   }
