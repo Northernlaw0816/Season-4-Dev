4a3f56848b09741298001da6ea1763182923180f
RishiMenon2004 <rishimenon82004@gmail.com>
2022-07-10 21:08:14 +0530

    stoopid

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -1,22 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { randomBytes, scryptSync, timingSafeEqual } from "crypto";
-import "dotenv/config";
-import { initializeApp } from "firebase/app";
-import { addDoc, collection, deleteField, doc, getDocs, getFirestore, query, setDoc, updateDoc, where, orderBy } from "firebase/firestore";
-import _ from "lodash";
 import fs from "fs-extra";
 import nodemailer from "nodemailer";
 import nunjucks from "nunjucks";
-const app = initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-});
-const firestore = getFirestore(app);
+
 const htmlBody = fs.readFileSync("mail.html", "utf8");
 
 // Mail Provider
