64fef7ffb5149c0171b6f246abf365856e955490
RishiMenon2004 <rishimenon82004@gmail.com>
2022-07-19 02:47:30 +0530

    formatting

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,186 +1,206 @@
-import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
-import type { NextApiRequest, NextApiResponse } from "next";
+import {
+	addDoc,
+	collection,
+	getDocs,
+	query,
+	where
+} from "firebase/firestore";
+
+import type { NextApiRequest, NextApiResponse} from "next";
 
 import { firestore } from "../../firebase/clientApp";
 import { titleCase } from "../../functions";
 
 export default async function register(req: NextApiRequest, res: NextApiResponse) {
-  let body = req.body;
+	let body = req.body;
 
-  const { event, participants, team, platform, schoolId } = body;
+	const {
+		event,
+		participants,
+		team,
+		platform,
+		schoolId
+	} = body;
 
-  let success = true;
-  let message = "";
-  if (team) {
-    message = `Successfully registered for ${titleCase(event.toString().replaceAll("-", " "))}`;
-  } else if (participants) {
-    message = `Successfully registered for ${titleCase(event.toString().replaceAll("-", " "))}`;
-  }
+	let success = true;
+	let message = "";
+	
+	if (team) {
+		message = `Successfully registered for ${titleCase(event.toString().replaceAll("-", " "))}`;
+	} else if (participants) {
+		message = `Successfully registered for ${titleCase(event.toString().replaceAll("-", " "))}`;
+	}
 
-  if (!req.body) {
-    return res.status(200).json({
-      success: false,
-      message: "No body",
-    });
-  }
+	if (!req.body) {
+		return res.status(200).json({
+			success: false,
+			message: "No body",
+		});
+	}
 
-  // Database code here
+	// Database code here
 
-  const registrationsCollection = collection(firestore, "registrations_season_2");
-  const allRegistrationsSnapshot = await getDocs(registrationsCollection);
-  const registrationsSnapshot = await getDocs(query(registrationsCollection, where("schoolId", "==", schoolId)));
-  const schoolRegistrations: any = [];
-  const allRegistrations: any = [];
+	const registrationsCollection = collection(firestore, "registrations_season_2");
+	const allRegistrationsSnapshot = await getDocs(registrationsCollection);
+	const registrationsSnapshot = await getDocs(query(registrationsCollection, where("schoolId", "==", schoolId)));
+	const schoolRegistrations: any = [];
+	const allRegistrations: any = [];
 
-  registrationsSnapshot.forEach((doc: any) => {
-    schoolRegistrations.push(doc);
-  });
-  allRegistrationsSnapshot.forEach((doc: any) => {
-    allRegistrations.push(doc);
-  });
+	registrationsSnapshot.forEach((doc: any) => {
+		schoolRegistrations.push(doc);
+	});
+	allRegistrationsSnapshot.forEach((doc: any) => {
+		allRegistrations.push(doc);
+	});
 
-  const phones: any = [];
-  if (allRegistrations.length > 0) {
-    allRegistrations.forEach((registrations: any) => {
-      if (registrations.get("team")) {
-        registrations.get("team").participants.forEach((participant: any) => {
-          phones.push(participant.phone);
-        });
-      }
+	const phones: any = [];
+	if (allRegistrations.length > 0) {
+		allRegistrations.forEach((registrations: any) => {
+			if (registrations.get("team")) {
+				registrations.get("team").participants.forEach((participant: any) => {
+					phones.push(participant.phone);
+				});
+			}
 
-      if (registrations.get("participants")) {
-        registrations.get("participants").forEach((participant: any) => {
-          phones.push(participant.phone);
-        });
-      }
-    });
+			if (registrations.get("participants")) {
+				registrations.get("participants").forEach((participant: any) => {
+					phones.push(participant.phone);
+				});
+			}
+		});
 
-    if (team) {
-      team.participants.forEach((participant: any, index: number) => {
-        if (phones.includes(participant.phone)) {
-          return res.status(200).json({ success: false, message: `Phone number already in use (Participant: ${index + 1})` });
-        }
-      });
-    }
+		if (team) {
+			team.participants.forEach((participant: any, index: number) => {
+				if (phones.includes(participant.phone)) {
+					return res.status(200).json({
+						success: false,
+						message: `Phone number already in use (Participant: ${index + 1})`
+					});
+				}
+			});
+		}
 
-    if (participants) {
-      participants.forEach((participant: any, index: number) => {
-        if (participant.phone === "") delete participants[index];
-        if (phones.includes(participant.phone)) {
-          return res.status(200).json({ success: false, message: `Phone number already in use (Participant: ${index + 1})` });
-        }
-      });
-    }
-  }
+		if (participants) {
+			participants.forEach((participant: any, index: number) => {
+				if (participant.phone === "") delete participants[index];
+				if (phones.includes(participant.phone)) {
+					return res.status(200).json({
+						success: false,
+						message: `Phone number already in use (Participant: ${index + 1})`
+					});
+				}
+			});
+		}
+	}
 
-  if (schoolRegistrations.length > 0) {
-    const tmpEvents: any = [];
-    const teamName: any = [];
+	if (schoolRegistrations.length > 0) {
+		const tmpEvents: any = [];
+		const teamName: any = [];
 
-    schoolRegistrations.forEach((registration: any) => {
-      if (registration.get("event") === "arena-of-valor") {
-        tmpEvents.push(`${registration.get("event")}:${registration.get("platform")}`);
-      } else tmpEvents.push(registration.get("event"));
-    });
-const filteredEvents = tmpEvents.filter((Event:any)=>event === Event)
-    if (event === "arena-of-valor") {
-      if (tmpEvents.filter((Event: any) => `${event}:${platform}` === Event).length >= 3) {
-        return res.status(200).json({
-          success: false,
-          message: `Reached maximum registrations for this platform (${platform})`,
-        });
-      }
-    } else if (tmpEvents.filter((Event: any) => event === Event).length >=3) {
-      return res.status(200).json({
-        success: false,
-        message: `Reached maximum registrations for this event`,
-      });
-    }
+		schoolRegistrations.forEach((registration: any) => {
+			if (registration.get("event") === "arena-of-valor") {
+				tmpEvents.push(`${registration.get("event")}:${registration.get("platform")}`);
+			} else tmpEvents.push(registration.get("event"));
+		});
 
-    if (team) {
-      let teamNames: Array<any> = [];
-      if (!teamNames.includes(team.teamName) || !teamName.includes(team.teamName)) {
-        teamNames.push(team.teamName);
-        teamName.push(team.teamName);
-      } else {
-        return res.status(200).json({
-          success: false,
-          message: `Team name already registered`,
-        });
-      }
-      team.participants.forEach((participant: any, index: any) => {
-        let formPhones: Array<any> = [];
-        let formNames: Array<any> = [];
+		if (event === "arena-of-valor") {
+			if (tmpEvents.filter((Event: any) => `${event}:${platform}` === Event).length >= 3) {
+				return res.status(200).json({
+					success: false,
+					message: `Reached maximum registrations for this platform (${platform}) from your school`,
+				});
+			}
+		} else if (tmpEvents.filter((Event: any) => event === Event).length >= 3) {
+			return res.status(200).json({
+				success: false,
+				message: `Reached maximum registrations for this event from your school`,
+			});
+		}
 
-        team.participants.forEach((participant: any, innerIndex: any) => {
-          if (index !== innerIndex) {
-            if (participant.phone !== "") {
-              formPhones.push(participant.phone);
-            }
-            if (participant.name !== "") {
-              formNames.push(participant.name);
-            }
-          }
-        });
+		if (team) {
+			let teamNames: Array < any > = [];
+			if (!teamNames.includes(team.teamName) || !teamName.includes(team.teamName)) {
+				teamNames.push(team.teamName);
+				teamName.push(team.teamName);
+			} else {
+				return res.status(200).json({
+					success: false,
+					message: `Team name already registered`,
+				});
+			}
+			team.participants.forEach((participant: any, index: any) => {
+				let formPhones: Array < any > = [];
+				let formNames: Array < any > = [];
 
-        if (phones.includes(participant.phone)) {
-          success = false;
-          message = `Phone number already in use (Participant ${index + 1} : ${participant.phone})`;
-        } else if (formPhones.includes(participant.phone)) {
-          success = false;
-          message = `Phone numbers cannot be same as other members (Participant ${index + 1} : ${participant.phone})`;
-        }
-      });
-    } else {
-      participants.forEach((participant: any, index: any) => {
-        let formPhones: any = [];
-        let formNames: any = [];
+				team.participants.forEach((participant: any, innerIndex: any) => {
+					if (index !== innerIndex) {
+						if (participant.phone !== "") {
+							formPhones.push(participant.phone);
+						}
+						if (participant.name !== "") {
+							formNames.push(participant.name);
+						}
+					}
+				});
 
-        participants.forEach((participant: any, innerIndex: any) => {
-          if (index !== innerIndex) {
-            formPhones.push(participant.phone);
-            formNames.push(participant.name);
-          }
-        });
+				if (phones.includes(participant.phone)) {
+					success = false;
+					message = `Phone number already in use (Participant ${index + 1} : ${participant.phone})`;
+				} else if (formPhones.includes(participant.phone)) {
+					success = false;
+					message = `Phone numbers cannot be same as other members (Participant ${index + 1} : ${participant.phone})`;
+				}
+			});
+		} else {
+			participants.forEach((participant: any, index: any) => {
+				let formPhones: any = [];
+				let formNames: any = [];
 
-        if (phones.includes(participant.phone)) {
-          success = false;
-          message = `Phone number already in use (Participant ${index + 1} : ${participant.phone})`;
-        } else if (formPhones.includes(participant.phone)) {
-          success = false;
-          message = `Phone numbers cannot be same as participants (Participant ${index + 1} : ${participant.phone})`;
-        }
-      });
-    }
-  }
+				participants.forEach((participant: any, innerIndex: any) => {
+					if (index !== innerIndex) {
+						formPhones.push(participant.phone);
+						formNames.push(participant.name);
+					}
+				});
 
-  const data: any = {
-    event,
-    schoolId,
-  };
+				if (phones.includes(participant.phone)) {
+					success = false;
+					message = `Phone number already in use (Participant ${index + 1} : ${participant.phone})`;
+				} else if (formPhones.includes(participant.phone)) {
+					success = false;
+					message = `Phone numbers cannot be same as participants (Participant ${index + 1} : ${participant.phone})`;
+				}
+			});
+		}
+	}
 
-  if (team) {
-    data["team"] = team;
-  }
+	const data: any = {
+		event,
+		schoolId,
+	};
 
-  if (participants) {
-    data["participants"] = participants;
-  }
+	if (team) {
+		data["team"] = team;
+	}
 
-  if (platform) {
-    data["platform"] = platform;
-  }
+	if (participants) {
+		data["participants"] = participants;
+	}
 
-  delete data.userToken;
+	if (platform) {
+		data["platform"] = platform;
+	}
 
-  if (success) {
-    addDoc(registrationsCollection, {
-      ...data,
-    });
-  }
+	delete data.userToken;
 
-  res.status(200).json({
-    success,
-    message,
-  });
+	if (success) {
+		addDoc(registrationsCollection, {
+			...data,
+		});
+	}
+
+	res.status(200).json({
+		success,
+		message,
+	});
 }
