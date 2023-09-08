cdbedc598f00cc7bfaac2df0ddc19e155941735c
NuTopia Season 3 <144292482+NuTopiaS3@users.noreply.github.com>
2023-09-08 10:37:45 +0530

    Update Main.tsx
    Changed event date and season/year

--- data/Main.tsx
+++ data/Main.tsx
@@ -1,12 +1,12 @@
 const date = new Date('July 20 2022 21:00:00 GMT+0530')
 
 const Main = {
-    year: 2022,
-    season: 2,
+    year: 2023,
+    season: 3,
     tagline: "A Hub For Cyber Geeks!",
     dates: {
-        day1: new Date(2022, 6, 22).toDateString().split(" ").slice(1).join(" "),
-        day2: new Date(2022, 6, 23).toDateString().split(" ").slice(1).join(" ")
+        day1: new Date(2023, 9, 20).toDateString().split(" ").slice(1).join(" "),
+        day2: new Date(2023, 9, 21).toDateString().split(" ").slice(1).join(" ")
     },
     registrationClosingDate: date, // month is 0 indexed; Jan is 0;
     brochureDownload: "https://drive.google.com/u/0/uc?id=1LochKjLSKWeb0_rC4IUECCPCxMtjS3_w&export=download",
