df1f5f79ff5b33ec39a74c24d296a731de963079
NuTopia Season 3 <144292482+NuTopiaS3@users.noreply.github.com>
2023-09-08 22:54:37 +0530

    Update otakuiz.tsx
    added few rules otk

--- data/pages/events/otakuiz.tsx
+++ data/pages/events/otakuiz.tsx
@@ -31,9 +31,13 @@ const Otakuiz = {
         about: "Welcome to Otakuiz, the ultimate competition to crown the true, most worthy team of the title “Super Sannin”. It’s finally your time to prove that all those hours spent watching anime are not in vain by showing off the knowledge you’ve gained in the ultimate anime quiz. Are you ready to give Otakuiz a shot? Meet fellow weebs and have the time of your life? So join us for the ride and let’s have a blast together.",
             
         rules: [
-            "The competitors will participate in groups of 3 and will each come up with a name for your team.",
-            "No use of the internet or any gadgets will be allowed.",
-            "Participants will be allowed to confer within their own teams to answer questions as there will be no individual participation rounds. However, they may not confer with any other groups in the middle of any rounds."
+            "The competitors will participate in groups of 2 or 3 and will each come up with a name for their respective team.",
+            "Use of the internet and electronic gadgets are strictly prohibited.",
+            "Participants will be allowed to confer within their own teams to answer questions as there will be no individual participation rounds. However, they may not confer with any other groups in the middle of any rounds.",
+            "Participants are not allowed to bring any stationary except pens.",
+            "The Otakuiz will consist of three rounds, the first being the preliminaries.",
+            "The preliminaries will not include any negative marking and contestants who cleared the preliminaries will move to the advancing rounds.",
+            "The second round is a geoguessr round."
         ],
         
         eventCoordinators: EventCoordinators.filter(coordinator => coordinator.event === "Otakuiz"),
