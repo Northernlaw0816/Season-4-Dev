0aaa1b0c0ea69f5d793a768eb664e9072c7802f2
NuTopia Season 3 <144292482+NuTopiaS3@users.noreply.github.com>
2023-10-18 14:24:31 +0530

    Update otakuiz.tsx

--- data/pages/events/otakuiz.tsx
+++ data/pages/events/otakuiz.tsx
@@ -43,7 +43,7 @@ const Otakuiz = {
         eventCoordinators: EventCoordinators.filter(coordinator => coordinator.event === "Otakuiz"),
 
         registration: [
-            "Participants register Trio Teams (3 members) - 3 teams per school",
+            "Participants register Trio Teams (3 members) - 5 teams per school",
             "Anime themed team names are encouraged. The team with the BEST name will receive a special prize.",
         ]
     }
