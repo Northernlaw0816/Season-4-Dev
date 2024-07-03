5749a4311e9612c15ec9d942df45e289119564ce
NuTopia Season 3 <144292482+NuTopiaS3@users.noreply.github.com>
2024-07-03 13:23:54 +0530

    update event dates

--- data/Main.tsx
+++ data/Main.tsx
@@ -5,8 +5,8 @@ const Main = {
     season: 4,
     tagline: "A Hub For Cyber Geeks!",
     dates: {
-        day1: new Date(2024, 7, 25).toDateString().split(" ").slice(1).join(" "),
-        day2: new Date(2024, 7, 26).toDateString().split(" ").slice(1).join(" ")
+        day1: new Date(2024, 6, 25).toDateString().split(" ").slice(1).join(" "),
+        day2: new Date(2024, 6, 26).toDateString().split(" ").slice(1).join(" ")
     },
     registrationClosingDate: date, // month is 0 indexed; Jan is 0;
     brochureDownload: "https://drive.google.com/u/0/uc?id=10re6i_oqC3SXiTLW5HEefl-bqKkeToRS&export=download",
