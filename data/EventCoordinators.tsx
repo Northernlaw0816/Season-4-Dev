865a0bd8a15d63d7770232c917d701b088e2aa2b
Northernlaw0816 <rithik.abhay8d@gmail.com>
2024-07-04 20:49:09 +0530

    RISHI HELPK

--- data/EventCoordinators.tsx
+++ data/EventCoordinators.tsx
@@ -1,11 +1,11 @@
 // Event Heads
-//import thrivikram from "../public/images/photos/Thrivikram.jpeg"
-//import rithik from "../public/images/photos/Rithik.jpeg"
+import thrivikram from "../public/images/photos/Thrivikram.jpeg"
+import rithik from "../public/images/photos/Rithik.jpeg"
 //Teachers Incharge
-//import aj from "../public/images/photos/aj.jpeg"
-//import vk from "../public/images/photos/vk.jpeg"
+import aj from "../public/images/photos/aj.jpeg"
+import vk from "../public/images/photos/vk.jpeg"
 // AOV
-//import akshay from "../public/images/photos/Akshay.jpeg"
+import akshay from "../public/images/photos/Akshay.jpeg"
 import abhinav from "../public/images/photos/Abhinav.jpeg"
 import abhishek from "../public/images/photos/Abhishek.jpeg"
 import shafiq from "../public/images/photos/Shafiq.jpeg"
@@ -25,16 +25,16 @@ import ksharaa from "../public/images/photos/Ksharaa.jpeg"
 // LAB
 import aadees from "../public/images/photos/Aadees.jpeg"
 import krishita from "../public/images/photos/Krishita.jpeg"
-//import shruti from "../public/images/photos/Shruti.jpeg"
+import shruti from "../public/images/photos/Shruti.jpeg"
 // DS
 import hari from "../public/images/photos/Hari.jpeg"
-// import arnav from "../public/images/photos/Arnav.jpeg"
-//import mahathi from "../public/images/photos/Mahathi.jpeg"
+import arnav from "../public/images/photos/Arnav.jpeg"
+import mahathi from "../public/images/photos/Mahathi.jpeg"
 // OTK
 import harsha from "../public/images/photos/Harsha.jpeg"
 import nebin from "../public/images/photos/Nebin.jpeg"
-//import tejasvi from "../public/images/photos/Tejasvi.jpeg"
-//import mithun from "../public/images/photos/Mithun.jpeg"
+import tejasvi from "../public/images/photos/Tejasvi.jpeg"
+import mithun from "../public/images/photos/Mithun.jpeg"
 // CC
 import maanya from "../public/images/photos/Maanya.jpeg"
 import prathiksha from "../public/images/photos/Prathiksha.jpeg"
@@ -44,9 +44,9 @@ import dhureen from "../public/images/photos/Dhureen.jpeg"
 import athul from "../public/images/photos/Athul.jpeg"
 import mayank from "../public/images/photos/Mayank.jpeg"
 // SM
-//import driti from "../public/images/photos/Driti.jpeg"
-//import dikshita from"../public/images/photos/Dikshita.jpeg"
-//import ananyaY from"../public/images/photos/AnanyaY.jpeg"
+import driti from "../public/images/photos/Driti.jpeg"
+import dikshita from"../public/images/photos/Dikshita.jpeg"
+import ananyaY from"../public/images/photos/AnanyaY.jpeg"
 
 //Place Holder Change later
 import placeholder from "../public/images/photos/placeholder.png";
