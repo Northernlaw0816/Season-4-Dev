81e7f003c31f94186f8fb22987f248e003b508a2
RishiMenon2004 <rishimenon82004@gmail.com>
2024-07-13 04:42:18 +0530

    close registrations

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -19,6 +19,7 @@ const Registration = () => {
         <Layout overrideClasses={styles.main}>
             <h1 id="title">{RegistrationData.title}</h1>
 
+            <h2 id="note" style={{fontSize: "2em", color: "#e30e35", textAlign: "center"}}>Registrations are temporarily closed due to technical difficulties.</h2>
             <p id="note" style={{fontSize: "1.5em"}}>NuTopia - Season 4 is open for schools in Coimbatore.</p>
             <p id="note" style={{fontSize: "1.5em"}}>Please read the given guidelines under each event before proceeding to fill the registration form.</p>
 
