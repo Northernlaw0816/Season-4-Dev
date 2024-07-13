b3d00007af02c5e03bd8eb64e075ebd34111859f
R-Rajaneesh <rajaneeshr2006@gmail.com>
2024-07-13 22:33:27 +0530

    🔄️ Refurbish Invitation Email.

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -1,39 +1,47 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import fs from "fs-extra";
-import nodemailer from "nodemailer";
+import Nodemailer from "nodemailer";
 import nunjucks from "nunjucks";
 
-const htmlBody = fs.readFileSync("mail.html", "utf8");
+const htmlBody = fs.readFileSync("public/templates/invite.njk").toString();
 
 // Mail Provider
-const Mail = nodemailer.createTransport({
-  host: process.env.MAILER_HOST,
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.MAILER_USER,
-    pass: process.env.MAILER_PASS,
-  },
-});
+const Mail = Nodemailer.createTransport({
+	host: "smtpout.secureserver.net",
+	secure: true,
+	secureConnection: false, // TLS requires secureConnection to be false
+	tls: {
+		ciphers: "SSLv3",
+	},
+	requireTLS: true,
+	port: 465,
+	debug: false,
+	auth: {
+		user: process.env.MAILER_EMAIL,
+		pass: process.env.MAILER_PASSWORD,
+	},
+} as Nodemailer.SendMailOptions);
 export default async function mail(req: NextApiRequest, res: NextApiResponse) {
-  let success = false;
-  if (!req.body)
-    return res.status(200).json({
-      success,
-      message: "No body",
-    });
-  Mail.sendMail({
-    from: '"NuTopia" <info@nutopia.in>',
-    to: req.body.userEmail,
-    cc: [`${process.env.MAILER_USER}`],
-    html: nunjucks.renderString(htmlBody, req.body.formData),
-  })
-    .then(() => (success = true))
-    .catch((e) => {
-      success = false;
-    });
-  res.status(200).json({
-    success,
-    message: `Successfully sent mail to ${req.body.userEmail}`,
-  });
+	let success = false;
+	if (!req.body)
+		return res.status(200).json({
+			success,
+			message: "No body",
+		});
+	const body: { schoolEmail: string; schoolName: string } = req.body;
+	await Mail.sendMail({
+		from: '"NuTopia" <info@nutopia.in>',
+		subject: `Nutopia Season 4 Official Invite`,
+		to: body.schoolEmail,
+		cc: [`${process.env.MAILER_EMAIL}`],
+		html: nunjucks.renderString(htmlBody, body),
+	})
+		.then(() => {
+			success = true;
+		})
+		.catch((e: any) => {});
+	res.status(200).json({
+		success,
+		message: `Successfully sent mail to ${body.schoolEmail}`,
+	});
 }
