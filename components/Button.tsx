6eb81381d6e95c7ff725253b6af21ad7f8906244
Northernlaw0816 <rithik.abhay8d@gmail.com>
2024-07-17 16:29:42 +0530

    fullscreen stuff

--- components/Button.tsx
+++ components/Button.tsx
@@ -9,13 +9,40 @@ const Button: React.FC<{ text?: string; children?: React.ReactNode; href?: strin
   children,
   href,
 }) => {
-  const { open } = createPopup('UBaQ2F2X');
-
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    e.preventDefault(); // Prevent default anchor tag behavior (scrolling to top)
+    e.preventDefault(); // Prevent default anchor tag behavior
+
+    // Open the page in fullscreen mode
+    const docEl = document.documentElement;
+    if (docEl.requestFullscreen) {
+      docEl.requestFullscreen();
+    } else if ((docEl as any).mozRequestFullScreen) { // Firefox
+      (docEl as any).mozRequestFullScreen();
+    } else if ((docEl as any).webkitRequestFullscreen) { // Chrome, Safari and Opera
+      (docEl as any).webkitRequestFullscreen();
+    } else if ((docEl as any).msRequestFullscreen) { // IE/Edge
+      (docEl as any).msRequestFullscreen();
+    }
+
+    const { open } = createPopup('UBaQ2F2X', {
+      onSubmit: () => exitFullscreen(),
+      onClose: () => exitFullscreen()
+    });
 
     // Open the Typeform popup
     open();
+  };
+
+  const exitFullscreen = () => {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if ((document as any).mozCancelFullScreen) { // Firefox
+      (document as any).mozCancelFullScreen();
+    } else if ((document as any).webkitExitFullscreen) { // Chrome, Safari and Opera
+      (document as any).webkitExitFullscreen();
+    } else if ((document as any).msExitFullscreen) { // IE/Edge
+      (document as any).msExitFullscreen();
+    }
   };
 
   return (
